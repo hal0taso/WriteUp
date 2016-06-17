0df0d93226b7f1698e2f7db234c402bfd462871f
hal0taso <yuya7719guitar@gmail.com>
2016-06-17 11:48:26 +0900

    bit modified! dont care

--- atcoder/rhd/rhd_a.py
+++
@@ -1,8 +0,0 @@
-n, m = map(int, input().split())
-
-ans = set((map(int,input().split())))
-num = set([x for x in range(1,m+1)])
-
-diff = list(num.difference(ans))
-
-print(' '.join(map(str,sorted(diff))))
